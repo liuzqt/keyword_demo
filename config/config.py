4cbcecd964d1d00d98d9965438d8cefb24b2eca6
liuzqt <liuzq12@hotmail.com>
2017-06-20 11:30:46 +0800

    temp

--- config/config.py
+++ config/config.py
@@ -20,9 +20,11 @@ def get_config():
 
 class Config(object):
     def __init__(self):
-        self.mode = "valid"  # train,valid
+        self.mode = "train"  # train,valid
         self.max_pooling_loss = False
         self.max_pooling_standardize = True
+        self.ktq = False
+        self.optimizer = 'adam'  # adam sgd nesterov
         self.spectrogram = 'mel'  # mfcc,mel
         self.label_id = 0  # nihaolele,lele,whole
         self.label_list = ['nihaolele', 'lele', 'whole']
@@ -31,17 +33,13 @@ class Config(object):
 
         self.reset_global = 0
 
-        self.model_path = './params/mel_max/'
-        self.save_path = './params/mel_max/'
+        self.model_path = './params/mel_all/'
+        self.save_path = './params/mel_all/'
         self.graph_path = './graph/mel/'
         self.graph_name = 'graph.pb'
         self.data_path = './data/mel/'
 
-        # self.model_path = './params/lele_mfcc/'
-        # self.working_path = './params/lele_mfcc/'
-        # self.data_path = './data/lele_mfcc/'
-
-        # self.data_path = '/ssd/liuziqi/mel_all5x/'
+        self.data_path = '/ssd/liuziqi/mel_all5x/'
         self.model_name = 'best.ckpt'
         self.rawdata_path = './rawdata/'
         self.rawdata_path = '/ssd/keyword/'
@@ -55,19 +53,21 @@ class Config(object):
         self.cell_clip = 3.
         self.num_layers = 1
         self.init_scale = 0.1
-        self.learning_rate = 5e-5
+        self.learning_rate = 5e-4
         self.max_grad_norm = 5
         self.num_features = 60
         self.hidden_size = 64
         self.use_project = False
         self.num_proj = 32
         self.max_epoch = 200
-        self.keep_prob = 1.0
-        self.grad_clip = -1
-        self.lr_decay = 0.5
+        self.drop_out_input = -1
+        self.drop_out_output = -1
+        self.grad_clip = 0
+        self.lr_decay = 0.9
+        self.decay_step = 20000
         self.batch_size = 32
         self.tfrecord_size = 32
-        self.trigger_threshold = 0.7  # between (0,1), but this param is somehow arbitrary
+        self.trigger_threshold = 0.6  # between (0,1), but this param is somehow arbitrary
 
         # these three sizes are frames, which depend on STFT frame size
         self.smoothing_window = 9
